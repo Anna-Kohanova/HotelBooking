46f596b67e4cf27114c1150fd072e3a845e73068
Anna-Kohanova <anitacx88@gmail.com>
2017-04-25 09:47:41 +0300

    Add room form validation

--- client/src/app/admin/hotelrooms/add/add-hotelrooms.component.ts
+++ client/src/app/admin/hotelrooms/add/add-hotelrooms.component.ts
@@ -1,9 +1,22 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+
 @Component({
     selector: 'add-hotelrooms-component',
     templateUrl: './add-hotelrooms.component.html'
 })
 
 export class AddHotelRoomsComponent {
+    roomsForm: FormGroup;
+
+    ngOnInit(): void {
+        this.roomsForm = new FormGroup({
+            name: new FormControl('', Validators.required),
+            price: new FormControl('', Validators.required),
+            rating: new FormControl('', Validators.required),
+            visitors: new FormControl('', Validators.required),
+            description: new FormControl('', Validators.required)
+        });
+    }
 
 }
